ce87d0bc1033870a7374a2b79359f8affd1d3cbc
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2017-10-06 18:43:18 +0300

    Added the function and an example

---
+++ example_script.m
@@ -0,0 +1,34 @@
+figure(1)
+clf
+hold
+N=7000;
+a=randn(N,1);
+
+
+str.data=a;
+
+b=randn(N,1)+3;
+str.data2=b;
+str.location=1
+
+h=violin_funct(str,0.02,20);
+
+str2.data=a;
+str2.location=2;
+h=violin_funct(str2,0.02,20);
+
+str2.data=a;
+str2.location=3;
+h=violin_funct(str2,0.02,20,[0.5 1 0.5]);
+
+
+str2.data=a;
+str2.location=4;
+h=violin_funct(str2,0.02,1,[0.5 1 0.5]);
+
+str2.data=a;
+str2.location=5;
+h=violin_funct(str2,0.5,1,[1 1 0]);
+
+ylabel('Distribution')
+title('Example use of violin plot')
